c85623c8e4cb82a93853c1407ccdd0e7e032370b
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2023-06-29 23:18:14 +0300

    create item model form

--- items/views.py
+++ items/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 
-from items.forms import ItemForm
+from items.model_forms import ItemModelForm
 from items.models import Item
 
 
 def items(request, *args, **kwargs):
 
     if request.method == 'POST':
-        form = ItemForm(request.POST, files=request.FILES)
+        form = ItemModelForm(request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
     else:
-        form = ItemForm()
+        form = ItemModelForm()
     context = {
         'items': Item.objects.all(),
         'form': form
