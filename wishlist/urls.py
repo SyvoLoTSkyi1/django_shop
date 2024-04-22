21ec6c1854039daf9e74a6d7f5482afe9870401d
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2024-04-22 11:19:23 +0300

    refactoring previous changes, some adds to templates

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from wishlist.views import WishlistView, UpdateWishlistView
 
 urlpatterns = [
-    path('wishlist/', WishlistView.as_view(), name='wishlist'),
-    # re_path(r'wishlist/(?P<action>add|remove)/',
-    #         UpdateWishlistView.as_view(),
-    #         name='update_wishlist'),
+    path('wishlist/',
+         WishlistView.as_view(),
+         name='wishlist'),
     path('wishlist/<uuid:pk>/',
          UpdateWishlistView.as_view(),
          name='update_wishlist'),
--- wishlist/views.py
+++ wishlist/views.py
@@ -2,56 +2,15 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, RedirectView, DetailView, ListView
+from django.views.generic import DetailView, ListView
 
 from items.models import Item
-from wishlist.forms import UpdateWishlistForm
-from wishlist.mixins import GetWishlistMixin
 from wishlist.models import WishlistItem
-
-
-# class WishlistView(GetWishlistMixin, TemplateView):
-#     template_name = 'wishlist/items_wishlist.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super(WishlistView, self).get_context_data(**kwargs)
-#         context.update({
-#             'wishlist': self.get_wishlist_object()
-#         })
-#         return context
-
-
-# class UpdateWishlistView(GetWishlistMixin, RedirectView):
-#
-#     def post(self, request, *args, **kwargs):
-#         form = UpdateWishlistForm(request.POST,
-#                                   instance=self.get_wishlist_object())
-#         if form.is_valid():
-#             if kwargs['action'] == 'remove':
-#                 messages.warning(request,
-#                                  message='Item was deleted from your wishlist!'
-#                                  )
-#             else:
-#                 messages.success(request,
-#                                  message='Item was add to your wishlist!')
-#             form.save(kwargs.get('action'))
-#         return self.get(request, *args, **kwargs)
-#
-#     def get_redirect_url(self, *args, **kwargs):
-#         return reverse_lazy(
-#             'wishlist')
 
 
 class WishlistView(LoginRequiredMixin, ListView):
     model = WishlistItem
     template_name = 'wishlist/items_wishlist.html'
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(WishlistView, self).get_context_data(**kwargs)
-    #     context.update({
-    #         'wishlist': self.get_wishlist_object()
-    #     })
-    #     return context
 
 
 class UpdateWishlistView(LoginRequiredMixin, DetailView):
@@ -67,4 +26,9 @@ class UpdateWishlistView(LoginRequiredMixin, DetailView):
         )
         if not created:
             wishlist.delete()
+            messages.warning(request,
+                            message='Item was deleted from your wishlist!')
+        else:
+            messages.success(request,
+                            message='Item was add to your wishlist!')
         return HttpResponseRedirect(reverse_lazy('items'))
