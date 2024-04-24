fb5310630f3337da9876e49896eab24b475b921e
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2024-04-24 13:00:08 +0300

    update wishlist ajax jquery

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from wishlist.views import WishlistView, UpdateWishlistView
+from wishlist.views import WishlistView, AjaxUpdateWishlistView
 
 urlpatterns = [
     path('wishlist/',
          WishlistView.as_view(),
          name='wishlist'),
-    path('wishlist/<uuid:pk>/',
-         UpdateWishlistView.as_view(),
-         name='update_wishlist'),
+    # path('wishlist/<uuid:pk>/',
+    #      UpdateWishlistView.as_view(),
+    #      name='update_wishlist'),
+    path('ajax-wishlist/<uuid:pk>/',
+         AjaxUpdateWishlistView.as_view(),
+         name='ajax_update_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,10 +1,12 @@
-from django.contrib import messages
+# from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
+from django.http import JsonResponse
+# from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, ListView
 
 from items.models import Item
+from shop.decorators import ajax_required
 from wishlist.models import WishlistItem
 
 
@@ -13,9 +15,33 @@ class WishlistView(LoginRequiredMixin, ListView):
     template_name = 'wishlist/items_wishlist.html'
 
 
-class UpdateWishlistView(LoginRequiredMixin, DetailView):
+# class UpdateWishlistView(LoginRequiredMixin, DetailView):
+#     model = Item
+#     template_name_suffix = '_wishlist'
+#
+#     def get(self, request, *args, **kwargs):
+#         item = self.get_object()
+#         user = request.user
+#         wishlist, created = WishlistItem.objects.get_or_create(
+#             item=item,
+#             user=user
+#         )
+#         if not created:
+#             wishlist.delete()
+#             messages.warning(request,
+#                             message='Item was deleted from your wishlist!')
+#         else:
+#             messages.success(request,
+#                             message='Item was add to your wishlist!')
+#         return HttpResponseRedirect(reverse_lazy('items'))
+
+
+class AjaxUpdateWishlistView(LoginRequiredMixin, DetailView):
     model = Item
-    template_name_suffix = '_wishlist'
+
+    @method_decorator(ajax_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         item = self.get_object()
@@ -26,9 +52,5 @@ class UpdateWishlistView(LoginRequiredMixin, DetailView):
         )
         if not created:
             wishlist.delete()
-            messages.warning(request,
-                            message='Item was deleted from your wishlist!')
-        else:
-            messages.success(request,
-                            message='Item was add to your wishlist!')
-        return HttpResponseRedirect(reverse_lazy('items'))
+
+        return JsonResponse({'created': created})
